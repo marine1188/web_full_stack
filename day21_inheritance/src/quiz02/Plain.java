1645ac16d0b0e91b9d0e8c8c1e78d7ac7a81000f
SEONG KYU CHO (조성규) <38072336+marine1188@users.noreply.github.com>
2021-04-06 17:23:22 +0900

    수정
    수정 함

--- day21_inheritance/src/quiz02/Plain.java
+++ day21_inheritance/src/quiz02/Plain.java
@@ -4,10 +4,10 @@ public class Plain extends Trans {
 	String name = null;
 	
 	public void start(String name) {
-		System.out.println( name +"출발 합니다");
+		System.out.println( name +"이륙 합니다");
 	}
 	public void stop(String name) {
-		System.out.println( name +"정지 합니다");
+		System.out.println( name +"착 합니다");
 	}
 
 }
