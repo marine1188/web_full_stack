4291496e765c75ee5d188a1a01a0d27083eae0a2
SEONG KYU CHO (조성규) <38072336+marine1188@users.noreply.github.com>
2021-04-06 17:25:06 +0900

    인터페이스 예제 2
    클레스 분활 하여 인터페이스 구현

---
+++ day22_interface_API/src/ex02/Interface/BB.java
@@ -0,0 +1,10 @@
+package ex02.Interface;
+
+public class BB implements IDraw {
+
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("bb클래스에서 인터페이스 메소드 ");
+	}
+
+}
---
+++ day22_interface_API/src/ex02/Interface/IDraw.java
@@ -0,0 +1,7 @@
+package ex02.Interface;
+
+public interface IDraw {
+	int su = 1000; //static final
+	public void draw();// abstract method
+
+}
