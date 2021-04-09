49c7e86e97738b07616d15c151b17d54f7370aab
SEONG KYU CHO (조성규) <38072336+marine1188@users.noreply.github.com>
2021-04-09 11:33:48 +0900

    입력 시스템 이용하기

---
+++ day25_io/src/ex02/IO/InputStreamTest.java
@@ -0,0 +1,22 @@
+package ex02.IO;
+
+public class InputStreamTest {
+	public static void main(String[] args) {
+		System.out.print("input = ");
+		
+		try {
+			int su;
+			
+			while( (su = System.in.read() ) != -1 ) {
+				
+				if( su == 10 ||  su == 13 || su == 32 ) continue;
+				if( su == 113 || su == 81 ) break;   // 'q'  || 'Q'
+				
+				char ch = (char)su;
+				System.out.println(ch);				
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
